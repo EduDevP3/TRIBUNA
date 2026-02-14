884ce2cecdbdc4ba1df6074fc5c6ba9aba957f75
EchauriMu <edechaurimu@ittepic.edu.mx>
2026-02-13 19:02:50 -0700

    refactor: migrar completamente de npm a pnpm

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -3,23 +3,12 @@ import { NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-const interRegular = fetch(new URL('./Inter-Regular.ttf', import.meta.url)).then((res) =>
-  res.arrayBuffer()
-);
-
-const interBold = fetch(new URL('./Inter-Bold.ttf', import.meta.url)).then((res) =>
-  res.arrayBuffer()
-);
+// Eliminamos los fetch de fuentes locales que pesan mucho
 
 export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
   try {
-    const [regularFont, boldFont] = await Promise.all([interRegular, interBold]);
-
     const { searchParams } = new URL(req.url);
-
-    const title = searchParams.has('title')
-      ? searchParams.get('title')?.slice(0, 100)
-      : process.env.SITE_NAME;
+    const title = searchParams.get('title')?.slice(0, 100) || process.env.SITE_NAME || 'Tribuna';
 
     return new ImageResponse(
       (
@@ -33,35 +22,17 @@ export async function GET(req: NextRequest): Promise<Response | ImageResponse> {
               d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
             />
           </svg>
-
           <div tw="mt-12 text-6xl text-white font-bold">{title}</div>
         </div>
       ),
       {
         width: 1200,
         height: 630,
-        fonts: [
-          {
-            name: 'Inter',
-            data: regularFont,
-            style: 'normal',
-            weight: 400
-          },
-          {
-            name: 'Inter',
-            data: boldFont,
-            style: 'normal',
-            weight: 700
-          }
-        ]
+        // Al no declarar 'fonts', Vercel usará su fuente Inter predeterminada que no ocupa espacio en tu cuota
       }
     );
-  } catch (e) {
-    if (!(e instanceof Error)) throw e;
-
+  } catch (e: any) {
     console.log(e.message);
-    return new Response(`Failed to generate the image`, {
-      status: 500
-    });
+    return new Response(`Failed to generate image`, { status: 500 });
   }
 }
