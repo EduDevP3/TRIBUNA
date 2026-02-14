6efccb339db626f172cd16cd51447f76d42cf140
EchauriMu <edechaurimu@ittepic.edu.mx>
2026-02-13 20:25:04 -0700

    fix: eliminar generador de imagenes OG problemático

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 
-import GitHubIcon from 'components/icons/github';
 import LogoIcon from 'components/icons/logo';
-import VercelIcon from 'components/icons/vercel';
 import { getMenu } from 'lib/products';
 
 const { SITE_NAME } = process.env;
@@ -25,7 +23,7 @@ export default async function Footer() {
             </a>
           </div>
           {menu.length ? (
-            <nav className="col-span-1 lg:col-span-7">
+            <nav className="col-span-1 lg:col-span-9">
               <ul className="grid md:grid-flow-col md:grid-cols-3 md:grid-rows-4">
                 {menu.map((item: { title: string; path: string }) => (
                   <li key={item.title} className="py-3 md:py-0 md:pb-4">
@@ -40,29 +38,11 @@ export default async function Footer() {
               </ul>
             </nav>
           ) : null}
-
-          <div className="col-span-1 text-black dark:text-white lg:col-span-2">
-            <a aria-label="Github Repository" href="https://github.com/vercel/commerce">
-              <GitHubIcon className="h-6" />
-            </a>
-          </div>
         </div>
         <div className="flex flex-col items-center justify-between space-y-4 pb-10 pt-6 text-sm md:flex-row">
           <p>
             &copy; {copyrightDate} {SITE_NAME}. Todos los derechos reservados.
           </p>
-          <div className="flex items-center text-sm text-white dark:text-black">
-            <span className="text-black dark:text-white">Creado por</span>
-            <a
-              rel="noopener noreferrer"
-              href="https://vercel.com"
-              aria-label="Vercel.com Link"
-              target="_blank"
-              className="text-black dark:text-white"
-            >
-              <VercelIcon className="ml-3 inline-block h-6" />
-            </a>
-          </div>
         </div>
       </div>
     </footer>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   eslint: {
-    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
   },
   images: {
